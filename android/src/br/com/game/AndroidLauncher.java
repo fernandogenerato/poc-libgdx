4823c2a76b85ff3c0044fb7643e6462264332721
Fernando <generatof@gmail.com>
2021-08-11 19:26:13 -0300

    initial commit

---
+++ android/src/br/com/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package br.com.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import br.com.game.GameApp;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameApp(), config);
+	}
+}
---
+++ desktop/src/br/com/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.com.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.com.game.GameApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameApp(), config);
+	}
+}
